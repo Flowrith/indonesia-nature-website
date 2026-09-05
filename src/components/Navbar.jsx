caf32177de689b338e40a34605bfdf1a90029bb1
Flowrith <Flowrencio423@gmail.com>
2026-09-05 19:50:12 +0800

    Added responsive navbar & working navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,26 @@
-    import '../styles/navbar.css';
-    import Logo from "../assets/Logo.png";
-    function Navbar() {
-        return(
+import { useState } from "react";
+import "../styles/navbar.css";
+import Logo from "../assets/Logo.png";
 
-    <nav className = "navbar">
-      <img src={Logo} alt="Logo" width="50" height="50"/>
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function hamburgerMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
+  return (
+    <nav className="navbar">
+      <img src={Logo} alt="Logo" width="50" height="50" />
       <h1>NATURE</h1>
-      <ul>
+      <button onClick={hamburgerMenu}>☰</button>
+      <ul className={menuOpen ? "active" : ""}>
         <li><a href="#home">HOME</a></li>
         <li><a href="#about">ABOUT</a></li>
         <li><a href="#contact">CONTACT</a></li>
-              <span><li><a href="">☰</a></li></span>
       </ul>
     </nav>
-)
+  );
 }
+
 export default Navbar;
